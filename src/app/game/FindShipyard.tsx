e85e636ccbc5e3e914b398c81cba95e8ee165b60
Tristan Rechenberger <rechenberger@me.com>
2024-02-06 17:57:45 +0100

    🎉

--- src/app/game/FindShipyard.tsx
+++ src/app/game/FindShipyard.tsx
@@ -1,22 +1,59 @@
-import { headers } from './page'
+import { api } from '@/server/api'
 
 export const FindShipyard = async ({
   systemSymbol,
 }: {
   systemSymbol: string
 }) => {
-  const result = (
-    await fetch(
-      `https://api.spacetraders.io/v2/systems/${systemSymbol}/waypoints?traits=SHIPYARD`,
-      {
-        headers,
-      },
-    ).then((res) => res.json())
-  ).data
+  const waypoints = await api.systems.getSystemWaypoints({
+    systemSymbol,
+    traits: 'SHIPYARD',
+  })
+
+  const shipyards = await Promise.all(
+    waypoints.data.map(async (waypoint) => {
+      const shipyard = (
+        await api.systems.getShipyard({
+          systemSymbol,
+          waypointSymbol: waypoint.symbol,
+        })
+      ).data
+      return {
+        waypoint,
+        shipyard,
+      }
+    }),
+  )
+
   return (
     <>
       <h2>Shipyards</h2>
-      <pre>{JSON.stringify(result, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(shipyards, null, 2)}</pre> */}
+      {shipyards.map((shipyard) => (
+        <div key={shipyard.waypoint.symbol}>
+          <h3>{shipyard.waypoint.symbol}</h3>
+          <h2>
+            {shipyard.waypoint.type} at ({shipyard.waypoint.x},{' '}
+            {shipyard.waypoint.y})
+          </h2>
+          <h2>
+            Shiptypes:{' '}
+            {shipyard.shipyard.shipTypes.map((t) => t.type).join(', ')}
+          </h2>
+          <h2>modificationsFee: {shipyard.shipyard.modificationsFee}</h2>
+          {/* <pre>SHIPS: {JSON.stringify(shipyard.shipyard.ships, null, 2)}</pre> */}
+          {/* <pre>{JSON.stringify(shipyard.shipyard.transactions, null, 2)}</pre> */}
+          <h2>Ships:</h2>
+          {shipyard.shipyard.ships?.map((ship, idx) => (
+            <div key={idx} className="border p-2">
+              <h3>{ship.name}</h3>
+              <h2>Price: {ship.purchasePrice}</h2>
+              {/* <h2>Modifications:</h2>
+              <pre>{JSON.stringify(ship.modifications, null, 2)}</pre> */}
+            </div>
+          ))}
+        </div>
+      ))}
     </>
   )
 }
