23df297083368b2a81cde1879feeb1cbd1a72d84
Tristan Rechenberger <rechenberger@me.com>
2024-02-06 16:34:58 +0100

    🎉

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from '@/components/ui/button'
+
 export default function Page() {
+  const register = async () => {
+    'use server'
+    console.log('hi server')
+  }
+
   return (
     <>
       <h2>Game</h2>
+      <form action={register}>
+        <Button type="submit">Register</Button>
+      </form>
     </>
   )
 }
