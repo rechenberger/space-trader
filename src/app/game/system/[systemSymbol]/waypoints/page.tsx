86696ba21cdf8e7c6ab02b76e96bd28ad8517218
Tristan Rechenberger <rechenberger@me.com>
2024-02-08 19:38:03 +0100

    💄 ui

--- src/app/game/system/[systemSymbol]/waypoints/page.tsx
+++ src/app/game/system/[systemSymbol]/waypoints/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { api } from '@/server/api'
+import { revalidatePath } from 'next/cache'
 
 export default async function Page({
   params,
@@ -23,13 +25,40 @@ export default async function Page({
         {waypoints.map((w) => (
           <Card
             key={w.waypoint.symbol}
-            className="p-4 text-xs flex flex-col gap-4"
+            className="p-4 text-xs flex flex-col gap-2"
           >
             <div className="flex flex-row justify-between">
-              <div className="font-bold">{w.waypoint.symbol}</div>
-              <div className="text-muted-foreground capitalize">
+              <div className="font-bold flex-1">{w.waypoint.symbol}</div>
+              <div className="text-muted-foreground">
+                {w.waypoint.x},{w.waypoint.y}
+              </div>
+              <div className="text-muted-foreground capitalize flex-1 text-right">
                 {w.waypoint.type.toLowerCase()}
               </div>
+            </div>
+            <div className="line-clamp-3 h-12">
+              {w.waypoint.traits.map((t) => t.name).join(', ')}
+            </div>
+            <hr className="my-2" />
+            <div className="flex flex-row justify-end">
+              <form
+                action={async () => {
+                  'use server'
+                  const shipSymbol = 'TINGO-3'
+                  const result = await api.fleet.navigateShip({
+                    shipSymbol,
+                    navigateShipRequest: {
+                      waypointSymbol: w.waypoint.symbol,
+                    },
+                  })
+                  console.log('set waypoint result', result)
+                  revalidatePath('/game')
+                }}
+              >
+                <Button type="submit" variant="secondary" size="sm">
+                  Navigate
+                </Button>
+              </form>
             </div>
           </Card>
         ))}
